af2ad9e7098b087d76f6702d29373de530f81236
MolaniTTo <arnau.molano.7e7@itb.cat>
2024-11-06 18:27:21 +0100

    movimiento con raton

--- Assets/PlayerMovment.cs
+++ Assets/PlayerMovment.cs
@@ -1,4 +1,44 @@
-using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Obtenir la direcció de moviment dels inputs de l'usuari amb normalized, pq si es vertical, el vector resultant seria més gran
+        Vector2 moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        // Moure el personatge en la direccio dels inputs
+        transform.position += (Vector3)moveDirection * speed * Time.deltaTime;
+
+        // Només miro la direccio del cursor per fer l'animació si el personatge es mou
+        if (moveDirection != Vector2.zero)
+        {
+            // Obtenir la posicio del ratoli dins del marc de la camara
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // Resto el vector que es genera de la posicio del ratolí amb la posicio que té el personatge per treure el vector de on mirara el cursor
+            Vector2 lookDirection = (mousePosition - transform.position).normalized;
+
+            // Passar els parametres al Blend Tree en base a la direccio del cursor
+            animator.SetFloat("AnimationDirectionX", lookDirection.x);
+            animator.SetFloat("AnimationDirectionY", lookDirection.y);
+        }
+
+        // Establir la velocitat de l'animació en base a la magnitud del vector de moviment
+        animator.SetFloat("Speed", moveDirection.magnitude);
+    }
+}
+
+
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -33,4 +73,4 @@ public class PlayerMovment : MonoBehaviour
 
 
 
-}
+}*/
