79dafacbdceb9e327be49dcdd9a260eca9bdbe81
MolaniTTo <arnau.molano.7e7@itb.cat>
2024-11-12 20:00:22 +0100

    sprites y anim de presonaje torreta

--- Assets/PlayerMovment.cs
+++ Assets/PlayerMovment.cs
@@ -4,6 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     private Animator animator;
+    public bool isDied = false;
 
     void Start()
     {
@@ -12,39 +13,56 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // Obtenir la direcció de moviment dels inputs de l'usuari amb normalized, pq si es vertical, el vector resultant seria més gran
-        Vector2 moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
-
-        // Moure el personatge en la direccio dels inputs
-        transform.position += (Vector3)moveDirection * speed * Time.deltaTime;
-
-        //si el clic dret del mouse esta presionat
-        bool isRightClickPressed = Input.GetMouseButton(1);
-
-        // Només miro la direccio del cursor per fer l'animació si faig clic dret, sino, es mou on marquen les tecles
-        if (isRightClickPressed) //Si es presiona el clic dret (apuntar)
+        if(isDied == false)
         {
-            // Obtenir la posicio del ratoli dins del marc de la camara
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // Obtenir la direcció de moviment dels inputs de l'usuari amb normalized, pq si es vertical, el vector resultant seria més gran
+            Vector2 moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
 
-            // Resto el vector que es genera de la posicio del ratolí amb la posicio que té el personatge per treure el vector de on mirara el cursor
-            Vector2 lookDirection = (mousePosition - transform.position).normalized;
+            // Moure el personatge en la direccio dels inputs
+            transform.position += (Vector3)moveDirection * speed * Time.deltaTime;
 
-            // Passar els parametres al Blend Tree en base a la direccio del cursor
-            animator.SetFloat("AnimationDirectionX", lookDirection.x);
-            animator.SetFloat("AnimationDirectionY", lookDirection.y);
+            //si el clic dret del mouse esta presionat
+            bool isRightClickPressed = Input.GetMouseButton(1);
+
+            // Només miro la direccio del cursor per fer l'animació si faig clic dret, sino, es mou on marquen les tecles
+            if (isRightClickPressed) //Si es presiona el clic dret (apuntar)
+            {
+                // Obtenir la posicio del ratoli dins del marc de la camara
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                // Resto el vector que es genera de la posicio del ratolí amb la posicio que té el personatge per treure el vector de on mirara el cursor
+                Vector2 lookDirection = (mousePosition - transform.position).normalized;
+
+                // Passar els parametres al Blend Tree en base a la direccio del cursor
+                animator.SetFloat("AnimationDirectionX", lookDirection.x);
+                animator.SetFloat("AnimationDirectionY", lookDirection.y);
+            }
+
+            //sino es controla default per les tecles WASD
+            else if (moveDirection != Vector2.zero)
+            {
+                animator.SetFloat("AnimationDirectionX", moveDirection.x);
+                animator.SetFloat("AnimationDirectionY", moveDirection.y);
+            }
+
+            // Establir la velocitat de l'animació en base a la magnitud del vector de moviment
+            animator.SetFloat("Speed", moveDirection.magnitude);
+
         }
 
-        //sino es controla default per les tecles WASD
-        else if(moveDirection!= Vector2.zero) 
-        {
-            animator.SetFloat("AnimationDirectionX", moveDirection.x);
-            animator.SetFloat("AnimationDirectionY", moveDirection.y);
-        }
-
-        // Establir la velocitat de l'animació en base a la magnitud del vector de moviment
-        animator.SetFloat("Speed", moveDirection.magnitude);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            animator.SetTrigger("Die");
+            isDied = true;
+        }
+    }
+
+
+
 }
 
 
