dcfd8188b9d08b50bb8da269b8e7b1e22ff56d99
MolaniTTo <arnau.molano.7e7@itb.cat>
2024-11-13 14:43:50 +0100

    Enemigo torreta con bullet mediante codigo

---
+++ Assets/Scripts/Enemy/Torreta/BulletEnemy.cs
@@ -0,0 +1,56 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BulletEnemy : MonoBehaviour
+{
+    public float speed = 5f;
+    public float maxDistance = 10f;
+    public GameObject explosionEffect;
+
+    private Vector3 startPosition;
+    private Vector2 moveDirection;
+    
+    void Start()
+    {
+        startPosition = transform.position; 
+    }
+
+    void Update()
+    {
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+
+        if(Vector3.Distance(startPosition, transform.position) > maxDistance)
+        {
+            Explode();
+        }
+       
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        moveDirection = direction.normalized;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") || collision.CompareTag("Obstacle"))
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        if(explosionEffect != null)
+        {
+            GameObject explosionInstance = Instantiate(explosionEffect, transform.position, Quaternion.identity);
+            Destroy(explosionInstance, 1.0f);
+        }
+
+        Destroy(gameObject);
+    }
+}
